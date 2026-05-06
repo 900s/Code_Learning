11fe663ec15364b0cf05ac78bf03f015308a38fb
900s <99709702+900s@users.noreply.github.com>
2026-05-06 17:32:51 +0800

    Delete Project A directory

--- Project A/第一章/01. 入门程序.py
+++
@@ -1,4 +0,0 @@
-# 需求: 往控制台中输出"Hello World", "Hello Python"
-print("Hello World")
-print("Hello Python")
-print("----------------")
--- Project A/第二章/03. 输入与输出.py
+++
@@ -1,5 +0,0 @@
-# input(提示语) ---> 获取键盘上输入的数据
-password = input("请输入取款密码: ")
-print(f"密码为{password}, 密码正确。")
-money = input("请输入取款金额: ")
-print(f"已取款{money}元, 剩余金额为{10000 - int(money)}元。")      # 输入均为str类型, 需转换
